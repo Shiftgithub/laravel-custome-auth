596610b442e0f7d18eb1e961c2f9664b00d1254a
shiftgithub <mhturanmohammad@gmail.com>
2024-10-20 16:53:37 +0600

    custome-authentication completed

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -2,15 +2,76 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Session;
 
 class AuthController extends Controller
 {
-    public function register() {
-        echo "<h1>Hello</h1>";
+    public function register(Request $request)
+    {
+        if ($request->isMethod('POST')) {
+            $request->validate([
+                'name' => 'required',
+                'email' => 'required|email|unique:users',
+                'phone' => 'required',
+                'password' => 'required',
+            ]);
+            User::create($request->all());
+            if (Auth::attempt([
+                'email' => $request->email,
+                'password' => $request->password
+            ])) {
+                return to_route('dashboard')->with('success', 'your are logged in successfully');
+            } else {
+                to_route('register');
+            }
+        }
+        return view('auth.register');
     }
-    public function login() {}
-    public function dashboard() {}
-    public function profile() {}
-    public function logout() {}
+    public function login(Request $request)
+    {
+        if ($request->isMethod('POST')) {
+            $request->validate([
+                'email' => 'required|email',
+                'password' => 'required'
+            ]);
+            if (Auth::attempt([
+                'email' => $request->email,
+                'password' => $request->password
+            ])) {
+                return to_route('dashboard')->with('success', 'your are logged in successfully');
+            } else {
+                return to_route('login')->with('error', 'Invalid login details...');
+            }
+        }
+        return view('auth.login');
+    }
+    public function dashboard()
+    {
+        return view('auth.dashboard');
+    }
+    public function profile(Request $request)
+    {
+        if ($request->isMethod('POST')) {
+            $validatedData = $request->validate([
+                'name' => 'required|string|max:255',
+                'phone' => 'required|string|max:15',
+            ]);
+
+            $user = auth()->user();
+
+            $user->update($validatedData);
+
+            return redirect()->back()->with('success', 'Profile updated successfully!');
+        }
+        return view('auth.profile');
+    }
+    public function logout()
+    {
+        session::flush();
+        Auth::logout();
+        return to_route('login')->with('success', 'Logged out successfully');
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -2,16 +2,28 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\Controller;
 use Illuminate\Container\Attributes\Auth;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+
 // Route::resource('auth', AuthController::class);
-// Route::get('register/', [AuthController::class, 'register'])->name('register');
-// Route::post('register/', [AuthController::class, 'register'])->name('register');
-Route::match(['get', 'post'], 'register/', [AuthController::class, 'register'])->name('register');
-Route::get('login/', [AuthController::class, 'login'])->name('login');
-Route::get('dashboard/', [AuthController::class, 'dashboard'])->name('dashboard');
-Route::get('profile/', [AuthController::class, 'profile'])->name('profile');
-Route::get('logout/', [AuthController::class, 'logout'])->name('logout');
+// Route::get('profile/', [AuthController::class, 'profile'])->name('profile');
+
+Route::group([
+    "middleware" => ["guest"]
+], function () {
+    // Register
+    Route::match(['get', 'post'], 'register/', [AuthController::class, 'register'])->name('register');
+    // Login
+    Route::match(['get', 'post'], '/', [AuthController::class, 'login'])->name('login');
+});
+Route::group([
+    "middleware" => ["auth"]
+], function () {
+    // Profile
+    Route::match(['get', 'post'], 'profile/', [AuthController::class, 'profile'])->name('profile');
+    // Dashboard
+    Route::get('dashboard/', [AuthController::class, 'dashboard'])->name('dashboard');
+    // Logout
+    Route::get('logout/', [AuthController::class, 'logout'])->name('logout');
+});
